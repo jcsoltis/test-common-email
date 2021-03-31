ef847b4114b06a3d573151c946794e4e686cf9ab
Jcsoltis <jcsoltis@umich.edu>
2021-03-31 15:52:25 -0400

    Add test cases for Email addBcc(String... emails)

---
+++ commons-email-1.3.2-src/src/test/java/org/apache/commons/mail/EmailTest.java
@@ -0,0 +1,52 @@
+package org.apache.commons.mail;
+
+import static org.junit.Assert.*;
+
+import java.util.Date;
+
+import javax.mail.internet.InternetAddress;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class EmailTest {
+
+	//static global usage variables to be used in setting email functions
+	private static final String[] TEST_EMAILS = { "ab@bc.com", "a.b@c.org",
+			"abcdefghijklmnopqrst@abcdefghijklmnopqrst.com.bd" };
+	private static final String[] EMPTY_EMAILS = null;
+
+	//concrete email object to be tested
+	private EmailConcrete emailConcrete;
+	
+	//Setup method to initialize concrete email
+	@Before
+	public void setupEmailTest() throws Exception {
+		emailConcrete = new EmailConcrete();
+	}
+
+	//Test Email addBcc(String... emails)
+	@Test
+	public void testAddBcc() throws Exception {
+		emailConcrete.addBcc(TEST_EMAILS);
+		int testBccSize = 3;
+		
+		assertEquals("AddBcc size", testBccSize, emailConcrete.getBccAddresses().size());
+	}
+	
+	//Test Email addBcc(String... emails) for null list
+	@Test
+	public void testAddBccEmpty() throws Exception {
+		try{     
+			emailConcrete.addBcc(EMPTY_EMAILS);
+			fail("AddBcc null list not working correctly");
+		}catch(EmailException e){}
+	}
+	
+	//Teardown method blank because nothing to really tear down
+	@After
+	public void teardownEmailTest() throws Exception {
+		
+	}
+}
